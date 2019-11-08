e8305b9bd4bd9a97a706c5cf849192948228e5e4
matheusgomes062 <47605309+matheusgomes062@users.noreply.github.com>
2019-11-08 10:32:38 -0300

    Update warmupExercise()

--- warmUpExercise.m
+++ warmUpExercise.m
@@ -1,6 +1,6 @@
 function A = warmUpExercise()
-%WARMUPEXERCISE Example function in octave
-%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+% WARMUPEXERCISE Example function in octave
+% A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
 
 A = [];
 % ============= YOUR CODE HERE ==============
