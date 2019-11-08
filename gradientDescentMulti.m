abe25508d7be597dbe369387ea2b40e4689e12f4
matheusgomes062 <47605309+matheusgomes062@users.noreply.github.com>
2019-11-08 10:48:28 -0300

    Update gradientDescentMulti.m

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -9,14 +9,6 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
-
     % create a copy of theta for simultaneous update.
     % theta_n = theta;
 
